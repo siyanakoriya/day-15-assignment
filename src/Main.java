d9c6daaa26baa3de21930ffb549213cd2068d352
siyanakoriya <siyanakoriya122@gmail.com>
2022-09-16 07:23:32 +0530

    Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyHashFunction func = new MyHashFunction("To be or not To be",4);
+        //func.deleteData("Paranoids");
+        func.showTable();
+
+    }
+
+}
---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyHashFunction func = new MyHashFunction("To be or not To be",4);
+        //func.deleteData("Paranoids");
+        func.showTable();
+
+    }
+
+}
