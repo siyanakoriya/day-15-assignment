e7f50bad7c583f5138e0a79663ce3975f55d938f
siyanakoriya <siyanakoriya122@gmail.com>
2022-09-16 07:28:41 +0530

    Main

---
+++ src/com/bridgelabz/MyNode.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+
+public class MyNode {
+    String data;
+    int frequency;
+    MyMapNode next;
+
+    public MyMapNode(String data) {
+        this.data = data;
+        this.frequency = +1;
+        this.next = null;
+    }
+
+
+}
